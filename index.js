722c9da7db70bc379ccfa102d4428b2d9f4960fb
ommarkhawaja1 <ommarkhawaja1@gmail.com>
2022-07-18 12:24:45 -0400

    working readme generator

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-// TODO: Include packages needed for this application
+
 const inquirer = require("inquirer")
 const fs = require('fs')
 const generateMarkdown = require("./utils/generateMarkdown.js")
-// TODO: Create an array of questions for user input
+
 const questions = [{
     type: "input",
     message: "What is the title of the project?",
-    name: "Title"
-},
-{
-    type: "input",
-    message: "What is the project about? Please provide a short description.",
-    name: "Description"
-},
-{
-    type: "input",
-    message: "Table of Contents",
-    name: "Table of Contents"
-},
-{
-    type: "input",
-    message: "What are the steps required to install this project? What dependencies did you use?",
-    name: "Installation"
-},
-{
-    type: "input",
-    message: "How is this app used?",
-    name: "Usage"
+    name: "title"
 },
 {
     type: "list",
     name: "license",
     message: "Please enter the license for your project.",
-    choices: ["MIT", "Apache", "GNU GPL", "None"],
+    choices: ["MIT", "Apache 2.0", "GPL v3.0", "None"],
+},
+{
+    type: "input",
+    message: "What is the project about? Please provide a short description.",
+    name: "description"
+},
+{
+    type: "input",
+    message: "What are the steps required to install this project? What dependencies did you use?",
+    name: "installation"
+},
+{
+    type: "input",
+    message: "How is this app used?",
+    name: "usage"
 },
 {
     type: "input",
     message: "Who contributed to this project?",
-    name: "Contributions"
+    name: "contributions"
+},
+{
+    type: "input",
+    message: "How do you test this project",
+    name: "test"
 },
 {
     type: "input",
@@ -47,10 +47,11 @@ const questions = [{
 {
     type: "input",
     message: "What is your email address?",
-    name: "Email"
-}];
+    name: "email"
+}
+];
 
-// TODO: Create a function to write README file
+
 function writeToFile(data) {
     return new Promise((resolve, reject) => {
         fs.writeFile("./output/README.md", data, (err) => {
@@ -66,7 +67,7 @@ function writeToFile(data) {
     });
 }
 
-// TODO: Create a function to initialize app
+
 function init() {
     inquirer.prompt(questions)
         .then(function (response) {
--- output/README.md
+++ output/README.md
@@ -1,3 +1,32 @@
-# undefined
-  https://spdx.org/licenses/MIT.html
+# fsfgsdf
 
+  https://spdx.org/licenses/Apache-2.0.html
+
+  ## Table of Contents
+
+  * [Description](#description)
+  * [License](#license)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Test](#test)
+  * [Questions](#questions)
+
+  ## Description
+  werwer
+  ## License
+  * This application is covered by ![LICENSE BADGE](https://img.shields.io/badge/license-Apache%20License%202.0-brightgreen?style=for-the-badge).
+  ## Installation
+  werwer
+  ## Usage
+  werwer
+  ## Contributing
+  sdfsf
+  ## Test
+  eqew
+
+  ## Questions:
+  ### If you have any questions, you may contact me at either
+  ### Github: https://github.com/qwe
+  ### or
+  ### Email: trtret
